a844722fdd7e7acd341eddb30657b8319750e4f5
Dan Høegh <dan.hoegh@gmail.com>
2019-12-13 10:03:05 +0100

    cut down

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,7 @@
 // JS by Dan Høegh
-// UCN MMD 2018
+// UCN MMD 2019
 
 
 document.getElementById('btnAlert').addEventListener('click', function() {
     alert('You are alerted!');
 });
-
-// // let's make it easier to manipulate the box
-// let elmbox = document.getElementById('box');
-
-// // add class "show" when button is clicked
-// document.getElementById('btnBoxShow').addEventListener('click', function() {
-//     elmbox.classList.add('show');
-// });    
-
-// // remove class "show" when button is clicked
-// document.getElementById('btnBoxHide').addEventListener('click', function() {
-//     elmbox.classList.remove('show');
-// });    
-
-// // toggle class show when button is clicked
-// document.getElementById('btnBoxToggle').addEventListener('click', function() {
-//     elmbox.classList.toggle('show');
-// });
-
-// // add background-color on mouse in
-// elmbox.addEventListener('mouseover', function() {
-//     elmbox.classList.toggle('changecolor');
-// });    
-
-// // remove background-color on mouse in
-// elmbox.addEventListener('mouseout', function() {
-//     elmbox.classList.toggle('changecolor');
-// });    
