bd245fd94fbe90cae151763a4ad9f18e7c40e899
Dan Høegh <dan.hoegh@gmail.com>
2019-12-15 20:30:25 +0100

    uncommented the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,30 +6,30 @@ document.getElementById('btnAlert').addEventListener('click', function() {
     alert('You are alerted!');
 });
 
-// // let's make it easier to manipulate the box
-// let elmbox = document.getElementById('box');
+// let's make it easier to manipulate the box
+let elmbox = document.getElementById('box');
 
-// // add class "show" when button is clicked
-// document.getElementById('btnBoxShow').addEventListener('click', function() {
-//     elmbox.classList.add('show');
-// });    
+// add class "show" when button is clicked
+document.getElementById('btnBoxShow').addEventListener('click', function() {
+    elmbox.classList.add('show');
+});    
 
-// // remove class "show" when button is clicked
-// document.getElementById('btnBoxHide').addEventListener('click', function() {
-//     elmbox.classList.remove('show');
-// });    
+// remove class "show" when button is clicked
+document.getElementById('btnBoxHide').addEventListener('click', function() {
+    elmbox.classList.remove('show');
+});    
 
-// // toggle class show when button is clicked
-// document.getElementById('btnBoxToggle').addEventListener('click', function() {
-//     elmbox.classList.toggle('show');
-// });
+// toggle class show when button is clicked
+document.getElementById('btnBoxToggle').addEventListener('click', function() {
+    elmbox.classList.toggle('show');
+});
 
-// // add background-color on mouse in
-// elmbox.addEventListener('mouseover', function() {
-//     elmbox.classList.toggle('changecolor');
-// });    
+// add background-color on mouse in
+elmbox.addEventListener('mouseover', function() {
+    elmbox.classList.toggle('changecolor');
+});    
 
-// // remove background-color on mouse in
-// elmbox.addEventListener('mouseout', function() {
-//     elmbox.classList.toggle('changecolor');
-// });    
+// remove background-color on mouse in
+elmbox.addEventListener('mouseout', function() {
+    elmbox.classList.toggle('changecolor');
+});    
