7be54c04ffe07c21ccb76f337b91132576b1d221
Dan Høegh <dan.hoegh@gmail.com>
2020-12-11 10:54:09 +0100

    refactoring
    queryselector

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,35 @@
 // JS by Dan Høegh
-// UCN MMD 2019
+// UCN MMD 2020
 
 
-document.getElementById('btnAlert').addEventListener('click', function() {
+document.querySelector('#btnAlert').addEventListener('click', function() {
     alert('You are alerted!');
 });
 
 // let's make it easier to manipulate the box
-let elmbox = document.getElementById('box');
+let elmBox = document.querySelector('#box');
 
 // add class "show" when button is clicked
-document.getElementById('btnBoxShow').addEventListener('click', function() {
-    elmbox.classList.add('show');
+document.querySelector('#btnBoxShow').addEventListener('click', function() {
+    elmBox.classList.add('show');
 });    
 
 // remove class "show" when button is clicked
-document.getElementById('btnBoxHide').addEventListener('click', function() {
-    elmbox.classList.remove('show');
+document.querySelector('#btnBoxHide').addEventListener('click', function() {
+    elmBox.classList.remove('show');
 });    
 
 // toggle class show when button is clicked
-document.getElementById('btnBoxToggle').addEventListener('click', function() {
-    elmbox.classList.toggle('show');
+document.querySelector('#btnBoxToggle').addEventListener('click', function() {
+    elmBox.classList.toggle('show');
 });
 
 // add background-color on mouse in
-elmbox.addEventListener('mouseover', function() {
-    elmbox.classList.toggle('changecolor');
+elmBox.addEventListener('mouseover', function() {
+    elmBox.classList.toggle('changecolor');
 });    
 
 // remove background-color on mouse in
-elmbox.addEventListener('mouseout', function() {
-    elmbox.classList.toggle('changecolor');
+elmBox.addEventListener('mouseout', function() {
+    elmBox.classList.toggle('changecolor');
 });    
